cba7189e43351bde3694555f192706c48a53a00b
Nerissa Taylor <taylornerissa@hotmail.com>
2019-06-10 18:47:52 -0400

    arrayandobjects

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -68,17 +68,19 @@ console.log(`Car 33 is a *car year goes here* *car make goes here* *car model go
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
 let lastCar = 0;
-console.log();
+console.log(inventory.car_make[-1]);
+console.log(lastCar.car_model[-1]);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
-console.log();
+inventory.car_model.sort();
+console.log(car_model);
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
-let carYears = [];
-console.log();
+let carYears = [inventory.car_years];
+console.log(carYears);
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
@@ -87,8 +89,15 @@ console.log();
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
-let BMWAndAudi = [];
+  let BMWAndAudi = [];
+  for(i = 0; i < inventory.length; i++){
+    if([i].car_make === "BMW" ||[i]. car_make ==="Audi"){
+      BMWAndAudi.push([i]);
+
+
 console.log();
 
+}
+};
 
 
--- assignments/objects.js
+++ assignments/objects.js
@@ -18,27 +18,56 @@ const example = {
 }
 
 // Write your intern objects here:
-
+const interns =[{
+  id: 0,
+  name: "Mitzi",
+  email: "mmelloy0@psu.edu",
+  gender: "F"
+},
+{id: 1,
+name: "Kennan",
+email: "kdiben1@tinypic.com",
+gender: "M"
+},
+{
+  id: 2,
+  name: "Keven",
+  email: "kmummery2@wikimedia.org",
+  gender: "M"
+},
+{
+  id: 3,
+  name: "Gannie",
+  email: "gmartinson3@illinois.edu",
+  gender: "M"
+},
+{
+  id: 4,
+  name: "Antonietta",
+  email: "adaine5@samsung.com",
+  gender: "F"
+}];
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-
+console.log(interns.name[0]);
 // Kennan's ID
-
+console.log(interns.id[1]);
 // Keven's email
-
+console.log(interns.email[2]);
 // Gannie's name
-
+console.log(interns.name[3]);
 // Antonietta's Gender
+console.log(interns.gender[4]);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
+ console.log(kennan.speak("Hello, my name is" + `${this.name}` + "!"));
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+console.log(antonietta.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
